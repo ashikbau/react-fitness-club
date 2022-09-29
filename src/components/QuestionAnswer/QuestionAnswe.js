f46436ea0195b9298f4ad2216325f0573826dd02
Your Name <you@example.com>
2022-09-29 11:28:07 +0200

    complete question part added

--- src/components/QuestionAnswer/QuestionAnswe.js
+++ src/components/QuestionAnswer/QuestionAnswe.js
@@ -8,6 +8,15 @@ const QuestionAnswe = () => {
                 <h3>How does react work?</h3>
                 <p>React creates the virtual Dom which is Javascrip presentation of this Dom. When <br /> updates are made Reacts compares the <br />
                 current Dom to the Virtual Dom and only updates the differences <br /> between the two. This is the way of working React</p>
+                <div>
+                    <h3>The difference between props and state?</h3>
+                    <p>State is mutable and set by parent component, on the other <br /> hand props are immutable and set by eventhandler</p>
+                </div>
+                <div>
+                    <h3>What are useEffects other than loading data from API?</h3>
+                    <p>The useEffect Hook allows you to perform side effects in <br />  your components.Some examples of side effects <br />
+                     fetching data, directly updating the DOM, and timers.</p>
+                </div>
             </div>
         </div>
     );
