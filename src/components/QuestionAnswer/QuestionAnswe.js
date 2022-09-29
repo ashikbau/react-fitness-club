b5cd9ee3726e4c5090161e011e6142bbe0a61e33
Your Name <you@example.com>
2022-09-29 11:13:35 +0200

    part of question added

--- src/components/QuestionAnswer/QuestionAnswe.js
+++ src/components/QuestionAnswer/QuestionAnswe.js
@@ -3,8 +3,12 @@ import './QuestionAnswer.css'
 
 const QuestionAnswe = () => {
     return (
-        <div>
-            
+        <div >
+            <div className='question-ans'>
+                <h3>How does react work?</h3>
+                <p>React creates the virtual Dom which is Javascrip presentation of this Dom. When <br /> updates are made Reacts compares the <br />
+                current Dom to the Virtual Dom and only updates the differences <br /> between the two. This is the way of working React</p>
+            </div>
         </div>
     );
 };
